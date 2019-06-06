4771a83bf61d89d9e524b3ae3e227816da5c2a07
keerthivasan123 <keerthivasan287@gmail.com>
2019-06-06 21:44:50 +0530

    added all routes for account

--- routes/account.js
+++ routes/account.js
@@ -2,22 +2,22 @@ const express = require ('express');
 const router = express.Router();
 
 // get a list of ninjas from the db
-router.get('/ninjas', function(req, res){
+router.get('/get', function(req, res){
     res.send({type: 'GET'});
 });
 
 // add a new ninja to the db
-router.post('/ninjas', function(req, res){
+router.post('/post', function(req, res){
     res.send({type: 'POST'});
 });
 
 // update a ninja in the db
-router.put('/ninjas/:id', function(req, res){
+router.put('/put/:id', function(req, res){
     res.send({type: 'PUT'});
 });
 
 // delete a ninja from the db
-router.delete('/ninjas/:id', function(req, res){
+router.delete('/delete/:id', function(req, res){
     res.send({type: 'DELETE'});
 });
 
