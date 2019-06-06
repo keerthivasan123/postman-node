768223353a654d4e101ebecec44455547925610c
keerthivasan123 <keerthivasan287@gmail.com>
2019-06-06 23:29:18 +0530

    Some minor changes in routes

--- routes/account.js
+++ routes/account.js
@@ -1,23 +1,26 @@
 const express = require ('express');
 const router = express.Router();
+const account = require('../models/account');
 
 // get a list of ninjas from the db
-router.get('/get', function(req, res){
+router.get('/get', function(req, res, next){
     res.send({type: 'GET'});
 });
 
 // add a new ninja to the db
-router.post('/post', function(req, res){
-    res.send({type: 'POST'});
+router.post('/post', function(req, res, next){
+    account.create(req.body).then(function(account){
+        res.send(account);
+    }).catch(next);
 });
 
 // update a ninja in the db
-router.put('/put/:id', function(req, res){
+router.put('/put/:id', function(req, res, next){
     res.send({type: 'PUT'});
 });
 
 // delete a ninja from the db
-router.delete('/delete/:id', function(req, res){
+router.delete('/delete/:id', function(req, res, next){
     res.send({type: 'DELETE'});
 });
 
--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,28 @@
 const express = require ('express');
 const router = express.Router();
+const user = require('../models/user');
 
 // get a list of ninjas from the db
-router.get('/get', function(req, res){
+router.get('/get', function(req, res, next){
     res.send({type: 'GET'});
 });
 
 // add a new ninja to the db
-router.post('/post', function(req, res){
-    res.send({type: 'POST'});
+router.post('/post', function(req, res, next){
+    req.body
+    user.create(req.body).then(function(user){
+
+        res.send(user);
+    }).catch(next);
 });
 
 // update a ninja in the db
-router.put('/put/:id', function(req, res){
+router.put('/put/:id', function(req, res, next){
     res.send({type: 'PUT'});
 });
 
 // delete a ninja from the db
-router.delete('/delete/:id', function(req, res){
+router.delete('/delete/:id', function(req, res, next){
     res.send({type: 'DELETE'});
 });
 
