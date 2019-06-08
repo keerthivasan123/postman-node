6b5851ec23748aba3419f312293bb655a4d77347
keerthivasan123 <keerthivasan287@gmail.com>
2019-06-08 14:38:00 +0530

    id added

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
+  _id: Schema.Types.ObjectId,
   email: String,
   password: String,
   account: [{ type: Schema.Types.ObjectId, ref: 'Account' }]
