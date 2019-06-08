1c67f951775209df8a41df15f63d8993fbf033f7
keerthivasan123 <keerthivasan287@gmail.com>
2019-06-08 14:38:40 +0530

    account and user  route changed

--- routes/account.js
+++ routes/account.js
@@ -2,26 +2,33 @@ const express = require ('express');
 const router = express.Router();
 const account = require('../models/account');
 
-// get a list of ninjas from the db
+// get a list of accounts from the db
 router.get('/get', function(req, res, next){
     res.send({type: 'GET'});
 });
 
-// add a new ninja to the db
+// add a new account to the db
 router.post('/post', function(req, res, next){
     account.create(req.body).then(function(account){
         res.send(account);
     }).catch(next);
 });
 
-// update a ninja in the db
+// update a account in the db
 router.put('/put/:id', function(req, res, next){
-    res.send({type: 'PUT'});
+    account.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+       account.findOne({_id: req.params.id}).then(function(account){
+            res.send(account);
+        });
+    }).catch(next);
 });
 
-// delete a ninja from the db
+// delete a account from the db
 router.delete('/delete/:id', function(req, res, next){
-    res.send({type: 'DELETE'});
+    console.log(req.params.id);
+    account.findByIdAndRemove({_id: req.params.id}).then(function(account){
+        res.send(account);
+    }).catch(next);
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,94 @@
 const express = require ('express');
 const router = express.Router();
-const user = require('../models/user');
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const Account = require('../models/account');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
-// get a list of ninjas from the db
-router.get('/get', function(req, res, next){
-    res.send({type: 'GET'});
+//Login
+router.post('/login', function(req, res, next){
+  const user = req.params.body;
+  jwt.sign({user}, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+// get a list of users from the db
+router.get('/details', function(req, res, next){
+    console.log(req.body.email);
+    const User = req.body;
+    user.findOne({ 'email':User.email,'password':User.password }).
+  populate('account').
+  exec(function (err, user) {
+    if (err) return err;
+    console.log("populated");
+    res.send(user);
+  });
 });
 
-// add a new ninja to the db
-router.post('/post', function(req, res, next){
-    req.body
-    user.create(req.body).then(function(user){
+// add a new user to the db
 
-        res.send(user);
+router.post('/post', function(req, res, next){
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    const { email, password, first_name,last_name} = req.body;
+    const user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      email,
+      password
+    });
+    
+    user.save(function (err) {
+      if (err) return handleError(err);
+    
+      const account = new Account({
+        first_name,
+        last_name   
+      });
+    
+      account.save(function (err) {
+        if (err) return handleError(err);
+        // thats it!
+      });
+    });
+});
+
+// update a user in the db
+router.put('/put/:id', function(req, res, next){
+    user.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+        user.findOne({_id: req.params.id}).then(function(user){
+            res.send(user);
+        });
     }).catch(next);
 });
 
-// update a ninja in the db
-router.put('/put/:id', function(req, res, next){
-    res.send({type: 'PUT'});
-});
-
-// delete a ninja from the db
+// delete a user from the db
 router.delete('/delete/:id', function(req, res, next){
-    res.send({type: 'DELETE'});
+    user.findByIdAndRemove({_id: req.params.id}).then(function(user){
+        res.send(user);
+    }).catch(next);
 });
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      next();
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+  
+  }
+  
 
 module.exports = router;
